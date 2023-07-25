fe1f8f7a154682c24752421cb1e9596a38cd45a0
Dexterlente <dexter.lente18@gmail.com>
2023-07-26 07:29:02 +0800

    push

--- google-hangouts/backend/socketServer.js
+++ google-hangouts/backend/socketServer.js
@@ -4,11 +4,22 @@ import { Server } from 'socket.io';
 function createSocketServer(server) {
   const io = new Server(server);
 
+  let connectedPeers = [];
   // Socket.IO connection handling
   io.on('connection', (socket) => {
-    // Your socket connection logic goes here
     console.log('User is connected on socketIO server')
-    console.log(socket.id)
+    connectedPeers.push(socket.id);
+    console.log(connectedPeers);
+    socket.on("disconnect", () =>{
+        console.log('User is disconnected');
+
+        const newconnectedPeers = connectedPeers.filter((peerSocketId) => {
+             // Return true for all socket IDs that are not equal to the disconnected client's socket ID
+            peerSocketId !== socket.id;
+        })
+         // Update the connectedPeers array to the new array, excluding the disconnected client's socket ID
+        connectedPeers = newconnectedPeers;
+    });
   });
 
   return io;
