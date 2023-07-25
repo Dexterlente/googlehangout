2999b9750a745940bcc25942efdcadaeefe64e7f
Dexterlente <dexter.lente18@gmail.com>
2023-07-25 23:10:12 +0800

    react-router-dom

--- google-hangouts/frontend/src/App.jsx
+++ google-hangouts/frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Route, Routes } from 'react-router-dom'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className='font-bold'>
-        HALLOW WOELRD
+      <Routes>
+            <Route path="/">
+              {/* <Route path="/" element={<MainBody />} /> */}
+              <Route path="/register" element={<Registration />} />
+              <Route path="/login" element={<Login />} />
+            </Route>                  
+          </Routes>
       </div>
       </>
   )
