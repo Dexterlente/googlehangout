6c148118ea58a88c258f6b9111cf0c8cd8666a82
Dexterlente <dexter.lente18@gmail.com>
2023-07-27 13:38:54 +0800

    Complex

--- google-hangouts/frontend/src/components/ContactFeed.jsx
+++ google-hangouts/frontend/src/components/ContactFeed.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GoCopy } from 'react-icons/go';
+import * as webRTC from './webRTC';
+import * as constants from './constants';
 
-const ContactFeed = ({socketId}) => {
-
+const ContactFeed = ({ socketId }) => {
+  const [calleePersonalCode, setCalleePersonalCode] = useState('');
 
   const handleOnClick = () => {
     navigator.clipboard.writeText(socketId)
     console.log('Socket ID copied:', socketId);
   };
+
+    // Function to handle the input change
+    const handleInputChange = (event) => {
+      // Access the input value and set it in the state variable (calleePersonalCode)
+      setCalleePersonalCode(event.target.value);
+    };
+
+
   const handleButtonChat = () => {
-    
     console.log('chat button clicked');
+
+    const callType = constants.callType.CHAT_PERSONAL_CODE;
+   
+    webRTC.sendPreOffer(callType, calleePersonalCode);
   };
+
+
   const handleButtonVideo = () => {
     
     console.log('video button clicked');
+    webRTC();
   };
 
 
@@ -41,7 +57,7 @@ const ContactFeed = ({socketId}) => {
               </div>
             </div>
             <div className='mt-24'>
-              <input className='p-2 rounded-lg' placeholder='Input Other Persons Code'/>
+              <input className='p-2 rounded-lg' placeholder='Input Other Persons Code' type='text' value={calleePersonalCode} onChange={handleInputChange}/>
             </div>
             <div className='mt-2'>
               <button className='rounded-xl bg-[#E0FFFF] px-4 py-2 mr-1' onClick={handleButtonChat}>Chat</button>
---
+++ google-hangouts/frontend/src/components/webRTC.jsx
@@ -0,0 +1,6 @@
+
+export const sendPreOffer = (callType, calleePersonalCode) => {
+    console.log('pre offer function executed')
+    console.log(callType);
+    console.log(calleePersonalCode);
+}
