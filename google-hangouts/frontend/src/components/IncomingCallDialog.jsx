72d6a7bd01e76207a4f1d13e05569ad63ba1f8af
Dexterlente <dexter.lente18@gmail.com>
2023-08-08 01:03:37 +0800

    D

--- google-hangouts/frontend/src/components/IncomingCallDialog.jsx
+++ google-hangouts/frontend/src/components/IncomingCallDialog.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { LuPhoneCall } from 'react-icons/lu';
+import { MdCallEnd } from 'react-icons/md';
 
 const IncomingCallDialog = ({ callTypeInfo, acceptCallHandler, rejectCallHandler }) => {
 
   return (
-    <div className="incoming-call-dialog">
+    <div className="flex items-center justify-center h-screen">
+      <div className="incoming-call-dialog bg-gray-200 p-4 rounded shadow-md">
       {/* Your dialog content goes here */}
-      <h2>Incoming Call</h2>
-      <p>Call Type: {callTypeInfo}</p>
-      <button onClick={acceptCallHandler}>Accept</button>
-      <button onClick={rejectCallHandler}>Reject</button>
+  
+      <h2 className='text-5xl font-bold m-5'>Incoming Call</h2>
+      <p className='text-xl font-bold m-5'>Call Type: {callTypeInfo}</p>
+      {/* <button onClick={acceptCallHandler}>Accept</button> */}
+      <div className='flex row items-center justify-center'>
+      <LuPhoneCall onClick={acceptCallHandler} className='text-6xl cursor-pointer mr-4 bg-green-500 rounded-full p-1' />
+      {/* <button onClick={rejectCallHandler}>Reject</button> */}
+      <MdCallEnd onClick={rejectCallHandler} className='text-6xl cursor-pointer ml-4 bg-red-500 rounded-full p-1' />
+      </div>
+      </div>
     </div>
   );
 };
--- google-hangouts/frontend/src/components/OutgoingCallDialog.jsx
+++ google-hangouts/frontend/src/components/OutgoingCallDialog.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MdCallEnd } from 'react-icons/md';
 // 
 const OutgoingCallDialog = ({ callTypeInfo, handleRejectCallOutGoing }) => {
 
@@ -9,12 +10,16 @@ const OutgoingCallDialog = ({ callTypeInfo, handleRejectCallOutGoing }) => {
   };
 
   return (
-    <div className="incoming-call-dialog">
-      Your dialog content goes here
-      <h2>Outgoing Call</h2>
+    <div className="flex items-center justify-center h-screen">
+    <div className="incoming-call-dialog bg-gray-200 p-4 rounded shadow-md">
+      <h2 className='text-5xl font-bold m-5'>Outgoing Call</h2>
       {/* <p>Call Type: {callTypeInfo} </p> */}
-      <button onClick={handleRejectCallOutGoing}>Reject</button>
+      {/* <button onClick={handleRejectCallOutGoing}>Reject</button> */}
       {/* <button >Reject</button> */}
+      <div className='flex row items-center justify-center'>
+      <MdCallEnd onClick={handleRejectCallOutGoing} className='text-6xl cursor-pointer ml-4 bg-red-500 rounded-full p-1' />
+      </div>
+      </div>
     </div>
   );
 };
