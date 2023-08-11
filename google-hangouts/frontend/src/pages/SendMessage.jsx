e5311f6af481ed483f20762311b52b6584ea0d17
Dexterlente <dexter.lente18@gmail.com>
2023-08-12 03:05:45 +0800

    D

--- google-hangouts/frontend/src/pages/SendMessage.jsx
+++ google-hangouts/frontend/src/pages/SendMessage.jsx
@@ -35,26 +35,30 @@ const SendMessage = () => {
 
   return (
     <div className='grid grid-cols-2'>
-      <div>
-        <h2>Send Message</h2>
-        <div>
-          <label>To: </label>
+      <div className='bg-gray-200 p-4 rounded shadow'>
+        <h2 className='text-xl font-semibold mb-2'>Send Message</h2>
+        <div className='mb-2'> 
+          <label className='block font-medium'>To: </label>
           <input
+            className='border rounded px-2 py-1 w-full'
             type="text"
             value={toNumber}
             onChange={(e) => setToNumber(e.target.value)}
           />
         </div>
-        <div>
-          <label>Message: </label>
+        <div className='mb-4'>
+          <label className='block font-medium'>Message: </label>
           <textarea
+            className='border rounded px-2 py-1 w-full'
             value={messageBody}
             onChange={(e) => setMessageBody(e.target.value)}
           />
         </div>
-        <button onClick={handleSendMessage}>Send Message</button>
-        {success === true && <p>Message sent successfully!</p>}
-        {success === false && <p>Message sending failed.</p>}
+        <button 
+        className='bg-[#FFD700] text-white px-4 py-2 rounded hover:bg-[#FFD704]'
+        onClick={handleSendMessage}>Send Message</button>
+        {success === true && <p className='text-green-600 mt-2' >Message sent successfully!</p>}
+        {success === false && <p className='text-red-600 mt-2' >Message sending failed.</p>}
       </div>
       <div>
         <IncomingMessages />
