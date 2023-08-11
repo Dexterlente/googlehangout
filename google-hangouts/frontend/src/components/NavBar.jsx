3c672ccb35cfdce67352b8b3fd4f966ba697343f
Dexterlente <dexter.lente18@gmail.com>
2023-08-12 02:58:00 +0800

    d

--- google-hangouts/frontend/src/components/NavBar.jsx
+++ google-hangouts/frontend/src/components/NavBar.jsx
@@ -3,11 +3,13 @@ import { Link, useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav className="bg-blue-500 p-4">
+    <nav className="bg-[#FFD700] p-4 mb-2">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-white text-xl font-bold">My App</Link>
         <ul className="flex space-x-4">
-          <li><Link to="/chat" className="text-white">Chat</Link></li>
+          <li><Link to="/" className="text-white">Peer to Peer</Link></li>
+          <li><Link to="/send" className="text-white">SMS</Link></li>
+          <li><Link to="/outbound" className="text-white">CALLS</Link></li>
         </ul>
       </div>
     </nav>
